23ce79d3a0368008ff80bece7e0da306d7b17cb9
Smoopi <Smoopi@users.noreply.github.com>
2025-10-08 22:09:21 +0000

    deploy: f138b35f8805e6252389d75a66bb847502fed5a8

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+const button = document.createElement("button");
+button.textContent = "Click me!";
+
+button.addEventListener("click", () => {
+  alert("Button clicked!");
+});
+
+document.body.appendChild(button);
